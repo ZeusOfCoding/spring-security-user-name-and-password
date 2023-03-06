1d2c6975f6737908613d9eee1ea161ddea9c6e72
L <blaisepaho@gmail.com>
2023-03-06 17:20:38 +0100

    first commit

---
+++ src/main/java/com/pnb/springsecurityusernameandpassword/SpringSecurityUserNameAndPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.pnb.springsecurityusernameandpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUserNameAndPasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityUserNameAndPasswordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pnb/springsecurityusernameandpassword/SpringSecurityUserNameAndPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnb.springsecurityusernameandpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUserNameAndPasswordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
